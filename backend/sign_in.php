8ce50d791fadb66e894aa99cf81b37bdd085beb4
nicolas dellazzeri <nicolasdellazzeri@gmail.com>
2022-03-23 23:28:16 -0300

    Auth complete

--- backend/sign_in.php
+++ backend/sign_in.php
@@ -1,12 +1,13 @@
 <?php
+  require __DIR__ . '/services/authenticateUserService.php';
   header("Content-Type: application/json;charset=utf-8");
 
   $request = file_get_contents('php://input');
   $user_data = json_decode($request);
 
   try {
-    $created_user = createUserService($user_data);
-    echo json_encode($created_user);
+    $jwt = authenticateUserService($user_data);
+    echo $jwt;
   } catch (Exception $err) {
     http_response_code($err->getCode());
     echo json_encode(array("error" => $err->getMessage()));
--- backend/src/services/authenticateUserService.php
+++ backend/src/services/authenticateUserService.php
@@ -1,16 +1,21 @@
 <?php
   function authenticateUserService($email, $password) {
     $rootDir = realpath($_SERVER["DOCUMENT_ROOT"]);
+    require $rootDir . '/utils/generateJWT.php';
     $USERS_PATH = $rootDir . '/files/users.json';
-
+    
     $users_file_content = createFileContents($USERS_PATH);
     $users = json_decode($users_file_content, true);
-
-    if(isset($email) && !empty($password) && isset($password) && !empty($password)) {
+    
+    if(isset($email) && !empty($email) && isset($password) && !empty($password)) {
       foreach($users as $user) {
         if($user["email"] == $email && password_verify($password, $user["password"])) {
-          throw new Exception('User already exists.', 409);
+          $headers = array('alg'=>'HS256','typ'=>'JWT');
+          $payload = array('sub'=>'1234567890','email'=>$email, 'exp'=>(time() + 60 * 60 * 24));
+          echo json_encode(array('jwt'=>generate_jwt($headers, $payload));
           break;
+        } else {
+          throw new Exception('Invalid email/password combination.', 409);
         }
       }
       throw new Exception('Incorrect email/password combination.', 401);
